bce3dd3aa2ecd9171a91be03caef9c918e2c8695
mmeo009 <shh020615@gmail.com>
2024-10-16 00:29:04 +0900

    [Feat] 문 콜라이더 추가

--- Avoid/Assets/Scripts/ClockRotation.cs
+++ Avoid/Assets/Scripts/ClockRotation.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class ClockRotation : MonoBehaviour
 {
     public float rotationSpeed = 5.0f;
+    public bool isRight = false;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        if (isRight)
+        {
+            transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+
     }
 }
